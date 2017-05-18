dc11adbac905f2a4d544a7d342fd1951f37ccedc
Niranjan <niranjanbodiga3535@gmail.com>
2017-05-18 18:21:31 +0530

    RabbitMq along with its cpu usage calculation

---
+++ RabbitMQcpuUsage.js
@@ -0,0 +1,43 @@
+var amqp = require("amqplib/callback_api");
+const os = require('os');
+const NUMBER_OF_CPUS = os.cpus().length;
+let startTime  = process.hrtime()
+let startUsage = process.cpuUsage()
+
+setInterval(() => {
+  // spin the CPU for 500 milliseconds
+  var now = Date.now()
+  amqp.connect("amqp://localhost",function(err,conn){
+    conn.createChannel(function(err,ch){
+      var q = "queue_name";
+      var msg = "this is the message string!!!";
+      ch.assertQueue(q,{durable: false});
+      ch.sendToQueue(q,new Buffer(msg),{persistent: false});
+      // console.log("time = "+time);
+    });
+  });
+
+  const newTime = process.hrtime();
+  const newUsage = process.cpuUsage();
+  const elapTime = process.hrtime(startTime)
+  const elapUsage = process.cpuUsage(startUsage)
+  startUsage = newUsage;
+  startTime = newTime;
+
+
+  const elapTimeMS = hrtimeToMS(elapTime)
+
+  const elapUserMS = elapUsage.user / 1000; // microseconds to milliseconds
+  const elapSystMS = elapUsage.system / 1000;
+  const cpuPercent = (100 * (elapUserMS + elapSystMS) / elapTimeMS / NUMBER_OF_CPUS).toFixed(1) + '%'
+
+  console.log('elapsed time ms:  ', elapTimeMS)
+  console.log('elapsed user ms:  ', elapUserMS)
+  console.log('elapsed system ms:', elapSystMS)
+  console.log('cpu percent:      ', cpuPercent, '\n')
+
+}, 1000);
+
+function hrtimeToMS (hrtime) {
+  return hrtime[0] * 1000 + hrtime[1] / 1000000
+}
