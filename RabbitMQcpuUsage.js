3413e98ef96b006358f10a1d6b5215aeedd812e4
Niranjan <niranjanbodiga3535@gmail.com>
2017-05-19 16:20:54 +0530

    working till 15k queues and then hanging

--- RabbitMQcpuUsage.js
+++ RabbitMQcpuUsage.js
@@ -22,45 +22,55 @@ fs.stat(__dirname+"/RabbitMQcpuStats.txt", function (err, stats) {
   });
 });
 
-var timer = setInterval(() => {
-  time+=1;
-  if(time>=limit){
-    clearInterval(timer);
+//RabbitMQ
+amqp.connect("amqp://localhost",function(err,conn){
+  if(err){
+    console.log('Connection closed-----------error connect');
+    return;
   }
-  var now = Date.now()
-  //RabbitMQ
-  amqp.connect("amqp://localhost",function(err,conn){
+  var timer = setInterval(() => {
+    time+=1;
+    if(time>=limit){
+      clearInterval(timer);
+    }
+    var now = Date.now()
+
     conn.createChannel(function(err,ch){
-      var q = "queue_name";
+      if(err){
+        console.log('Connection closed-----------error createChannel');
+        return;
+      }
+      var q = "queue_name"+time.toString();
+      // console.log(q);
       var msg = "this is the message string!!!";
       ch.assertQueue(q,{durable: false});
       ch.sendToQueue(q,new Buffer(msg),{persistent: false});
       // console.log("time = "+time);
     });
-  });
-  //RabbitMQ
 
-  const newTime = process.hrtime();
-  const newUsage = process.cpuUsage();
-  const elapTime = process.hrtime(startTime)
-  const elapUsage = process.cpuUsage(startUsage)
-  startUsage = newUsage;
-  startTime = newTime;
+    const newTime = process.hrtime();
+    const newUsage = process.cpuUsage();
+    const elapTime = process.hrtime(startTime)
+    const elapUsage = process.cpuUsage(startUsage)
+    startUsage = newUsage;
+    startTime = newTime;
 
 
-  const elapTimeMS = hrtimeToMS(elapTime)
+    const elapTimeMS = hrtimeToMS(elapTime)
 
-  const elapUserMS = elapUsage.user / 1000; // microseconds to milliseconds
-  const elapSystMS = elapUsage.system / 1000;
-  const cpuPercent = (100 * (elapUserMS + elapSystMS) / elapTimeMS / NUMBER_OF_CPUS).toFixed(1) + '%';
+    const elapUserMS = elapUsage.user / 1000; // microseconds to milliseconds
+    const elapSystMS = elapUsage.system / 1000;
+    const cpuPercent = (100 * (elapUserMS + elapSystMS) / elapTimeMS / NUMBER_OF_CPUS).toFixed(1) + '%';
 
-  // console.log('elapsed time ms:  ', elapTimeMS);
-  // console.log('elapsed user ms:  ', elapUserMS);
-  // console.log('elapsed system ms:', elapSystMS);
-  // console.log('cpu percent:      ', cpuPercent, '\n');
+    // console.log('elapsed time ms:  ', elapTimeMS);
+    // console.log('elapsed user ms:  ', elapUserMS);
+    // console.log('elapsed system ms:', elapSystMS);
+    // console.log('cpu percent:      ', cpuPercent, '\n');
 
-  fs.appendFile(__dirname+"/RabbitMQcpuStats.txt",new Buffer(cpuPercent+"\n"));
-}, 50);
+    fs.appendFile(__dirname+"/RabbitMQcpuStats.txt",new Buffer(cpuPercent.substr(0,cpuPercent.length-1)+","));
+  }, 10);
+});
+//RabbitMQ
 
 function hrtimeToMS (hrtime) {
   return hrtime[0] * 1000 + hrtime[1] / 1000000;
