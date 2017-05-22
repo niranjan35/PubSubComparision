2cf7a2317ac501e183e2d73542d9370d3d50d5a5
niranjan <niranjanbodiga3535@gmail.com>
2017-05-22 16:01:59 +0530

    RabbitMQ program running out of 1.6GB RAM for large no of msgs

--- RabbitMQcpuUsage.js
+++ RabbitMQcpuUsage.js
@@ -6,8 +6,9 @@ const NUMBER_OF_CPUS = os.cpus().length;
 let startTime  = process.hrtime()
 let startUsage = process.cpuUsage()
 
-var time = 0;
-var limit = 100000;
+var loop_count = 0;
+var lim_loop = 10000;
+var cycle_count = 1;
 
 fs.stat(__dirname+"/RabbitMQcpuStats.txt", function (err, stats) {
   console.log("checking if the write file exists and status is : "+stats);
@@ -22,56 +23,54 @@ fs.stat(__dirname+"/RabbitMQcpuStats.txt", function (err, stats) {
   });
 });
 
-//RabbitMQ
-amqp.connect("amqp://localhost",function(err,conn){
-  if(err){
-    console.log('Connection closed-----------error connect');
-    return;
-  }
-  var timer = setInterval(() => {
-    time+=1;
-    if(time>=limit){
-      clearInterval(timer);
+setTimeout(function(){
+
+  amqp.connect("amqp://localhost",function(err,conn){
+    if(err){
+      console.log("error in creatin conn object");
     }
-    var now = Date.now()
+    var timer = setInterval(function(){
+      var now = Date.now();
 
-    conn.createChannel(function(err,ch){
-      if(err){
-        console.log('Connection closed-----------error createChannel');
-        return;
-      }
-      var q = "queue_name"+time.toString();
-      // console.log(q);
-      var msg = "this is the message string!!!";
-      ch.assertQueue(q,{durable: false});
-      ch.sendToQueue(q,new Buffer(msg),{persistent: false});
-      // console.log("time = "+time);
-    });
+      conn.createChannel(function(err,ch){
+        if(err){
+          console.log("error in createChannel method ----");
+        }
+        var q = "queue_name "+loop_count.toString();
+        var msg = "Message";
+        ch.assertQueue(q,{durable: true});
+        ch.sendToQueue(q,new Buffer(msg),{persistent: true});
 
-    const newTime = process.hrtime();
-    const newUsage = process.cpuUsage();
-    const elapTime = process.hrtime(startTime)
-    const elapUsage = process.cpuUsage(startUsage)
-    startUsage = newUsage;
-    startTime = newTime;
+        //cpu calculations
+        const newTime = process.hrtime();
+        const newUsage = process.cpuUsage();
+        const elapTime = process.hrtime(startTime)
+        const elapUsage = process.cpuUsage(startUsage)
+        startUsage = newUsage;
+        startTime = newTime;
 
 
-    const elapTimeMS = hrtimeToMS(elapTime)
+        const elapTimeMS = hrtimeToMS(elapTime)
 
-    const elapUserMS = elapUsage.user / 1000; // microseconds to milliseconds
-    const elapSystMS = elapUsage.system / 1000;
-    const cpuPercent = (100 * (elapUserMS + elapSystMS) / elapTimeMS / NUMBER_OF_CPUS).toFixed(1) + '%';
+        const elapUserMS = elapUsage.user / 1000; // microseconds to milliseconds
+        const elapSystMS = elapUsage.system / 1000;
+        const cpuPercent = (100 * (elapUserMS + elapSystMS) / elapTimeMS / NUMBER_OF_CPUS).toFixed(1) + '%';
 
-    // console.log('elapsed time ms:  ', elapTimeMS);
-    // console.log('elapsed user ms:  ', elapUserMS);
-    // console.log('elapsed system ms:', elapSystMS);
-    // console.log('cpu percent:      ', cpuPercent, '\n');
+        fs.appendFile(__dirname+"/RabbitMQcpuStats.txt",new Buffer(cpuPercent.substr(0,cpuPercent.length-1)+","));
 
-    fs.appendFile(__dirname+"/RabbitMQcpuStats.txt",new Buffer(cpuPercent.substr(0,cpuPercent.length-1)+","));
-  }, 10);
-});
-//RabbitMQ
+        //clear interval if loop count is more
+        loop_count+=1;
+        if(loop_count>=lim_loop){
+          console.log("clearing timer");
+          clearInterval(timer);
+        }
+      });
 
-function hrtimeToMS (hrtime) {
-  return hrtime[0] * 1000 + hrtime[1] / 1000000;
-}
+    },10);
+  });
+
+  function hrtimeToMS (hrtime) {
+    return hrtime[0] * 1000 + hrtime[1] / 1000000;
+  }
+
+},500);
